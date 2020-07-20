fa05380923d6ee30127b1eb215fb5a298bbc5be2
Vr <veridetta@gmail.com>
2020-07-20 22:43:51 +0700

    New DB

--- db/mapping_db.sql
+++ db/mapping_db.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2.1
--- http://www.phpmyadmin.net
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 07, 2020 at 05:36 PM
--- Server version: 5.7.28-0ubuntu0.16.04.2
--- PHP Version: 7.2.26-1+ubuntu16.04.1+deb.sury.org+1
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 20 Jul 2020 pada 17.37
+-- Versi server: 10.4.13-MariaDB
+-- Versi PHP: 7.4.8
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -23,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `data`
+-- Struktur dari tabel `data`
 --
 
 CREATE TABLE `data` (
@@ -45,7 +46,7 @@ CREATE TABLE `data` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `data`
+-- Dumping data untuk tabel `data`
 --
 
 INSERT INTO `data` (`id`, `nama`, `hp`, `alamat`, `keahlian`, `pengalaman`, `pendidikan`, `ktp`, `poto`, `status`, `pekerjaan`, `partner`, `tanggal`, `lat`, `lng`) VALUES
@@ -54,7 +55,64 @@ INSERT INTO `data` (`id`, `nama`, `hp`, `alamat`, `keahlian`, `pengalaman`, `pen
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Struktur dari tabel `datapkh`
+--
+
+CREATE TABLE `datapkh` (
+  `id` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `ttl` text NOT NULL,
+  `alamat` text NOT NULL,
+  `kriteriawarga` text NOT NULL,
+  `kode_kriteriawarga` text NOT NULL,
+  `kriteriapkh` text NOT NULL,
+  `kode_kriteriapkh` text NOT NULL,
+  `tanggal` text NOT NULL,
+  `lat` text NOT NULL,
+  `lng` text NOT NULL,
+  `partner` text NOT NULL,
+  `poto` text NOT NULL,
+  `hp` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `datapkh`
+--
+
+INSERT INTO `datapkh` (`id`, `nama`, `ttl`, `alamat`, `kriteriawarga`, `kode_kriteriawarga`, `kriteriapkh`, `kode_kriteriapkh`, `tanggal`, `lat`, `lng`, `partner`, `poto`, `hp`) VALUES
+(1, 'Ahmad PKH', 'Cirebon, 15 Mei 2011', 'Argasunya, Cirebon', 'Kurang Mampu', '2', 'Anak SMA/MA atau sederajat', '3', '20-07-2020-20-59-14', '-6.766', '108.540', '1', '20-07-2020-20-59-14iconlogo.png', '0817798989');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `datawarga`
+--
+
+CREATE TABLE `datawarga` (
+  `id` int(11) NOT NULL,
+  `nama` text NOT NULL,
+  `ttl` text NOT NULL,
+  `alamat` text NOT NULL,
+  `penghasilan` text NOT NULL,
+  `partner` text NOT NULL,
+  `poto` text NOT NULL,
+  `tanggal` text NOT NULL,
+  `lat` text NOT NULL,
+  `lng` text NOT NULL,
+  `hp` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data untuk tabel `datawarga`
+--
+
+INSERT INTO `datawarga` (`id`, `nama`, `ttl`, `alamat`, `penghasilan`, `partner`, `poto`, `tanggal`, `lat`, `lng`, `hp`) VALUES
+(1, 'Ahmad Warga', 'Cirebon 18 Juli 2020', 'Argasunya, Cirebon', '1600000', '1', '20-07-2020-21-13-06download.png', '20-07-2020-21-13-06', '-6.766', '108.539', '0817798989');
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `users`
 --
 
 CREATE TABLE `users` (
@@ -71,7 +129,7 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `users`
+-- Dumping data untuk tabel `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `password`, `nama`, `hp`, `alamat`, `poto`, `role`, `status`, `tanggal`) VALUES
@@ -83,31 +141,58 @@ INSERT INTO `users` (`id`, `email`, `password`, `nama`, `hp`, `alamat`, `poto`, 
 --
 
 --
--- Indexes for table `data`
+-- Indeks untuk tabel `data`
 --
 ALTER TABLE `data`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `users`
+-- Indeks untuk tabel `datapkh`
+--
+ALTER TABLE `datapkh`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `datawarga`
+--
+ALTER TABLE `datawarga`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indeks untuk tabel `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT untuk tabel yang dibuang
 --
 
 --
--- AUTO_INCREMENT for table `data`
+-- AUTO_INCREMENT untuk tabel `data`
 --
 ALTER TABLE `data`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT untuk tabel `datapkh`
+--
+ALTER TABLE `datapkh`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- AUTO_INCREMENT untuk tabel `datawarga`
+--
+ALTER TABLE `datawarga`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT untuk tabel `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
